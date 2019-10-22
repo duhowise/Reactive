65acd7dc1134f1871aa0186ddaadb85ba6f98c44
duhowise <duhwise@gmail.com>
2019-10-22 16:08:13 +0000

    automatic unsubscribe

---
+++ RxSamples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace Reactive.RxSamples
+{
+
+    class Program
+    {
+        public Program()
+        {
+            var market = new Subject<float>();
+            market.Subscribe(
+                f=> Console.WriteLine($"price is {f}"),
+                ()=>Console.WriteLine("sequence is complete"));
+            market.OnNext(1.23f);
+            market.OnCompleted();
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+       
+    }
+}
