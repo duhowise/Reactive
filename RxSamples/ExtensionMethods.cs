1bb0b674465c37c39494a69232db5fb94e190a78
duhowise <duhwise@gmail.com>
2019-10-22 16:28:44 +0000

    proxy broadcasting

---
+++ RxSamples/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reactive;
+
+namespace RxSamples
+{
+    public static class ExtensionMethods
+    {
+        public static IDisposable Inspect<T>(this IObservable<T> self, string name)
+        {
+           return self.Subscribe(
+                x => Console.WriteLine(""),
+                ex=> Console.WriteLine($"{name} ha generated an exception {ex.Message}"),
+                ()=> Console.WriteLine($"{name} has completed"));
+           
+        }
+
+        public static IObserver<T> OnNext<T>(this IObserver<T> self, params T[] args)
+        {
+            foreach (var arg in args)
+           
+                self.OnNext(arg);
+            return self;
+            
+        }
+
+    }
+}
--- RxSamples/Program.cs
+++ RxSamples/Program.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Reactive.Subjects;
 
-namespace Reactive.RxSamples
+namespace RxSamples
 {
 
     class Program
     {
-        public Program()
-        {
-            var market = new Subject<float>();
-            market.Subscribe(
-                f=> Console.WriteLine($"price is {f}"),
-                ()=>Console.WriteLine("sequence is complete"));
-            market.OnNext(1.23f);
-            market.OnCompleted();
-        }
+        //public Program()
+        //{
+        //    var market = new Subject<float>();
+        //    market.Subscribe(
+        //        f=> Console.WriteLine($"price is {f}"),
+        //        ()=>Console.WriteLine("sequence is complete"));
+        //    market.OnNext(1.23f);
+        //    market.OnCompleted();
+        //}
         static void Main(string[] args)
         {
-            new Program();
+            var market=new Subject<float>();
+            var marketConsumer=new Subject<float>();
+            market.Subscribe(marketConsumer);
+            marketConsumer.Inspect("market consumer");
+            market.OnNext(1,2,3,4,5);
+            market.OnCompleted();
+            
         }
 
        
